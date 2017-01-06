15d50904badb44d9b378a9109c3fe4689813b38c
Garima Kamboj <garima.kamboj@icloud.com>
2017-01-06 05:32:40 +0530

    add elem function for picking word by word

--- parser.js
+++ parser.js
@@ -1,44 +1,60 @@
-var operators = ['+', '-', '*', '/']
-var keywords = ['if', 'define', 'begin', 'set!', 'lambda', 'quote']
-var inp
+var operators = ['+', '-', '*', '/', '>', '>=', '<', '<=']
+var keywords = ['if', 'define', 'begin', 'set!', 'lambda', 'quote', 'sqr', 'sqrt', 'equal?', 'number?']
+var next
 function parse (input, arr) {
   if (input.startsWith('(')) {
-    inp = parse(input.slice(1), [])
-    if (inp[0].slice(1) === "") {
-      return inp[1]
+    next = parse(input.slice(1), [])
+    if (next[0].slice(1) === "") {
+      return next[1]
     }
-    arr = arr.concat([inp[1]])
-    return parse(inp[0].slice(1), arr)
+    arr = arr.concat([next[1]])
+    return parse(next[0].slice(1), arr)
   }
   if (input.startsWith(')')) {
     return [input, arr]
   }
-  var input1
-  input1 = factory_parser(input).pop()
-  return parse(input1[1], arr.concat(input1[0]))
+  var result
+  result = factory_parser(input).pop()
+  return parse(result[1], arr.concat(result[0]))
 }
 
 function factory_parser (input) {
   var fnArr = [number_parser(input), opertor_parser(input), keyword_parser(input), space_parser(input)]
-  return fnArr.filter(function arrtype (value) {
+  return fnArr.filter(function (value) {
     return (typeof value === 'object')
   })
 }
 
+function elem(p) {
+  var str = ""
+  var sl = p[0]
+  while(sl !== " " && sl !== ')') {
+    str = str + sl
+    sl = p.slice(1)[0]
+    p = p.slice(1)
+  }
+  return [str, p]
+}
+
 function number_parser (input) {
-  return (!isNaN(Number(input[0]))) ? [input[0], input.slice(1)] : false
+  var first = elem(input)
+  return (!isNaN(Number(first[0]))) ? first : false
 }
 
 function opertor_parser (input) {
-  return (operators.indexOf(input[0]) !== -1) ? [input[0], input.slice(1)] : false
+  var first = elem(input)
+  return (operators.indexOf(first[0]) !== -1) ? first : false
 }
 
 function keyword_parser (input) {
-  return (keywords.indexOf(input[0]) !== -1) ? [input[0], input.slice(1)] : false
+  var first = elem(input)
+  return (typeof first[0] === 'string') ? first : false
 }
 
 function space_parser (input) {
   return (input[0] === ' ') ? [[], input.slice(1)] : false
 }
 
-console.log(parse('(+ (+ 3 4 (+ 7 8) 9 6) 2)', []))
+ console.log(parse('(+ (+ 3 4 (<= 7 8) 9 6) 2)', []))
+// console.log(parse('7', []))
+// console.log(parse('(define A 5)', []))
